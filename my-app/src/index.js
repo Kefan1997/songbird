5c1c52dc58aab664fc45a16c78e07408efedac27
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2021-01-04 18:28:28 +0300

    feat: add domain change to index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
 
+window.addEventListener('message', function(event) {
+  console.log(`Получено ${event.data} из ${event.origin}`);
+  localStorage.setItem('data', event.data);
+});
+
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
