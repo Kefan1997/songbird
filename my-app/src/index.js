2b82a0b347dcc4a51bb8463676e9cce7cd551b52
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2021-01-04 21:16:51 +0300

    feat: add more features to domain

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
 
-window.addEventListener('message', function(event) {
+localStorage.name = 'blabla';
+
+window.onmessage = function (event) {
+  if (event.origin !== 'kefan1997-resliv-domain2.netlify.app') {
+    return;
+  }
   console.log(`Получено ${event.data} из ${event.origin}`);
   localStorage.setItem('data', event.data);
-});
+  var payload = JSON.parse(event.data);
+  localStorage.setItem(payload.key, JSON.stringify(payload.data));
+};
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+window.onmessage = function (e) {
+  if (e.origin !== 'kefan1997-resliv-domain2.netlify.app') {
+    return;
+  }
+  let payload = JSON.parse(e.data);
+  switch (payload.method) {
+    case 'set':
+      localStorage.setItem(payload.key, JSON.stringify(payload.data));
+      break;
+    case 'get':
+      let parent = window.parent;
+      let data = localStorage.getItem(payload.key);
+      parent.postMessage(data, '*');
+      break;
+    case 'remove':
+      localStorage.removeItem(payload.key);
+      break;
+  }
+};
 
+ReactDOM.render(<Game />, document.getElementById('root'));
